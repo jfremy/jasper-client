9d558aad3021e921e86a827702739ecb42d53ebb
Jean-François Remy <jeff@melix.org>
2015-02-14 18:09:41 +0100

    French translation

--- client/modules/Unclear.py
+++ client/modules/Unclear.py
@@ -18,9 +18,9 @@ def handle(text, mic, profile):
                    number)
     """
 
-    messages = ["I'm sorry, could you repeat that?",
-                "My apologies, could you try saying that again?",
-                "Say that again?", "I beg your pardon?"]
+    messages = ["Peux-tu répéter?",
+                "Désolé, peux-tu réessayer?",
+                "Pardon?", "Désolé, je n'ai pas compris."]
 
     message = random.choice(messages)
 
